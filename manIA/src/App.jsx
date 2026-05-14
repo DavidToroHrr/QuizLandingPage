08351dc0ba7cdcbe3b0e2b2c3a0c5bcca9cede7f
DavidToroHrr <davidesteban0736@gmail.com>
2026-05-14 10:41:58 -0500

    components architecture example

--- manIA/src/App.jsx
+++ manIA/src/App.jsx
@@ -3,15 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import HeroComponent from './components/heroComponent'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <p>hola mundo
-        
-      </p>
+      <HeroComponent />
     </div>
   )
 }
---
+++ manIA/src/components/heroComponent.jsx
@@ -0,0 +1,9 @@
+export default function HeroComponent() {
+    return (
+        <div className="hero">
+            <h1>Bienvenido a QuizLandia</h1>
+            <p>¡Pon a prueba tus conocimientos con nuestros emocionantes quizzes!</p>
+            <button className="cta-button">Comenzar</button>
+        </div>
+    );
+}
